91c2e5d636c375f239af1f31f8472c56a04aab15
opuncture <opuncture.dbmanagement@gmail.com>
2023-12-25 20:02:45 +0530

    Intro screen layout desgin

---
+++ app/src/main/java/com/example/e_commerceapp/fragment/auth_fragments/IntroductionFragment.kt
@@ -0,0 +1,7 @@
+package com.example.e_commerceapp.fragment.auth_fragments
+
+import androidx.fragment.app.Fragment
+import com.example.e_commerceapp.R
+
+class IntroductionFragment: Fragment(R.layout.fragment_introduction) {
+}
